7d70d496df13cbc5508a64b930a345ff2dd924c5
lakors05 <lakors05@gmail.com>
2022-09-26 19:08:17 +0900

    데이터베이스 테이블 변경 - 각 테이블에 주키 추가, 자동번호 추가

--- db/ai.sql
+++ db/ai.sql
@@ -1,23 +1,25 @@
 DROP Table Member;
 
 CREATE TABLE member(
-    id     VARCHAR(30) NOT NULL COMMENT '회원ID',
-    pwd    VARCHAR(30) NOT NULL COMMENT '암호',
+    id       INT NOT NULL AUTO_INCREMENT  COMMENT '회원ID',
+    nickname VARCHAR(30) NOT NULL COMMENT '닉네임',
+    pwd      VARCHAR(30) NOT NULL COMMENT '암호',
     cre_date DATETIME NOT NULL COMMENT '가입일',
-    mod_date DATETIME NOT NULL COMMENT '마지막암호변경일'
+    mod_date DATETIME NOT NULL COMMENT '마지막암호변경일',
+    PRIMARY KEY (id)
 )
 COMMENT '회원기본정보';
 
-ALTER TABLE member
-    ADD CONSTRAINT member_pk_id 
-        PRIMARY KEY(id);
-        
+-- ALTER TABLE member
+--     ADD CONSTRAINT member_pk_id 
+--         PRIMARY KEY(id);
+
 -- CREATE UNIQUE INDEX member_uix_email
 --     ON member(email asc);
 
 -- NOW() : 현재 시간     
 
-INSERT INTO member(id,pwd,cre_date,mod_date)
+INSERT INTO member(nickname,pwd,cre_date,mod_date)
 VALUES ('iu','1111',NOW(),NOW());
 
 COMMIT;
@@ -25,39 +27,47 @@ COMMIT;
 SELECT * FROM member;
 
 -- ------------------------------
+-- B
+CREATE TABLE chatroom(
+    chatroomnum      INT NOT NULL COMMENT '채팅룸번호',
+    user_id          INT NOT NULL COMMENT '유저id',
+    chatroomcre_date DATETIME NOT NULL COMMENT '채팅룸생성일',
+    PRIMARY KEY (chatroomnum)
+)
+COMMENT '채팅룸정보';
 
 CREATE TABLE userchatting(
-    id             VARCHAR(30) NOT NULL COMMENT '회원ID',
+    id             INT NOT NULL AUTO_INCREMENT COMMENT '회원ID',
     chattingrecord VARCHAR(400) NOT NULL COMMENT '채팅기록',
-    chatroomnum    VARCHAR(30) NOT NULL COMMENT '채팅룸번호',
-    chatting_date DATETIME NOT NULL COMMENT '채팅일'
+    chatroomnum    INT NOT NULL COMMENT '채팅룸번호',
+    chatting_date  DATETIME NOT NULL COMMENT '채팅일',
+    PRIMARY KEY (id)
 )
 COMMENT '유저채팅정보';
 
 CREATE TABLE chatterchatting(
-    id             VARCHAR(30) NOT NULL COMMENT '채터ID',
+    id             INT NOT NULL AUTO_INCREMENT COMMENT '채터ID',
     chattingrecord VARCHAR(400) NOT NULL COMMENT '채팅기록',
-    chatroomnum    VARCHAR(30) NOT NULL COMMENT '채팅룸번호',
-    chatting_date DATETIME NOT NULL COMMENT '채팅일'
+    chatroomnum    INT NOT NULL COMMENT '채팅룸번호',
+    chatting_date  DATETIME NOT NULL COMMENT '채팅일',
+    PRIMARY KEY (id)
 )
 COMMENT '채터채팅정보';
 
 
+
+
+-- ---------------------------------------------------------------
 -- A
 CREATE TABLE chatroom(
-    chatroomnum    VARCHAR(30) NOT NULL COMMENT '채팅룸번호',
+    chatroomnum    VARCHAR(30) NOT NULL AUTO_INCREMENT COMMENT '채팅룸번호',
     chattingrecord VARCHAR(400) NOT NULL COMMENT '채팅기록',
     chatroomcre_date DATETIME NOT NULL COMMENT '채팅룸생성일'
 )
 COMMENT '채팅룸정보';
-chatroomchatroom
 
--- B
-CREATE TABLE chatroom(
-    chatroomnum    VARCHAR(30) NOT NULL COMMENT '채팅룸번호',
-    chatroomcre_date DATETIME NOT NULL COMMENT '채팅룸생성일'
-)
-COMMENT '채팅룸정보';
+
+
 
 
 
