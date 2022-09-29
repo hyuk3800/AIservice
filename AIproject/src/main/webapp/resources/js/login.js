f5e7d9b4f972a7751edddbaa3f6a2fd74acac584
javaai802 <shin67551728@gmail.com>
2022-09-29 19:48:04 +0900

    로그인페이지 수정

--- AIproject/src/main/webapp/resources/js/login.js
+++ AIproject/src/main/webapp/resources/js/login.js
@@ -1,38 +1,9 @@
-const userId = document.querySelector("#ID");
-const password = document.querySelector("#Password")
-const loginBTN = document.querySelector("#loginBTN")
+let container = document.getElementById('container')
 
-
-const postreq = () => {
-	console.log("클릭")
-//	span.innerText = "";
-	let xhr;
-	if (window.XMLHttpRequest) { // 모질라, 사파리, IE7+ ...
-        xhr = new XMLHttpRequest();
-    } else if (window.ActiveXObject) { // IE 6 이하
-        xhr = new ActiveXObject("Microsoft.XMLHTTP");
-    }
-	xhr.onreadystatechange = function(){
-		console.log(xhr.readyState);
-		if(xhr.readyState == 4){
-			console.log(xhr.status);
-			if(xhr.status == 200){
-
-			}
-		}
-	}
-	const id = userId.value;
-	const pwd = password.value;
-	xhr.open("POST", "/biz/login.do", true);
-	xhr.setRequestHeader("Content-type", "application/json");
-	let user = {
-		userid : id,
-		pwd : pwd
-	};
-
-	// console.log(user);
-	xhr.send(JSON.stringify(user));
-	
-};
-
-loginBTN.addEventListener('click', postreq);
+toggle = () => {
+  container.classList.toggle('sign-in')
+  container.classList.toggle('sign-up')
+}
+setTimeout(() => {
+  container.classList.add('sign-in')
+}, 200)
