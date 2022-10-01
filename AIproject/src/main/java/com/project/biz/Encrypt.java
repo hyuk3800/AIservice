47deb8536581482d49be31696329c7f7b9700589
leeinhyuk135 <leeinhyuk135@naver.com>
2022-10-01 17:29:40 +0900

    로그인 기능 구현
    로그인 기능 구현

---
+++ AIproject/src/main/java/com/project/biz/Encrypt.java
@@ -0,0 +1,68 @@
+package com.project.biz;
+
+import java.security.MessageDigest;
+import java.security.SecureRandom;
+
+public class Encrypt {
+	
+//	public String getSalt() {
+//		
+//		SecureRandom r = new SecureRandom();
+//		
+//		byte[] salt = new byte[20];
+//		
+//		r.nextBytes(salt);
+//		
+//		StringBuffer sb = new StringBuffer();
+//		for (byte b : salt) {
+//			sb.append(String.format("%02x", b));
+//		};
+//		
+//		System.out.println(sb.toString());
+//		
+//		return sb.toString();
+//	}
+	
+	
+	public String getEncrypt(String pwd, String salt) {
+		
+		String result = null;
+		
+		try {
+			MessageDigest md = MessageDigest.getInstance("SHA-256");
+			
+			md.update((pwd + salt).getBytes());
+			byte[] pwdsalt = md.digest();
+			
+			StringBuffer sb = new StringBuffer();
+			for (byte b : pwdsalt) {
+				sb.append(String.format("%02x", b));
+			}
+			
+			result = sb.toString();
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		return result;
+		
+	}
+
+
+//	public static void main(String[] args) {
+//		Encrypt en = new Encrypt();
+//		
+//		String pwd = "나는이인혁";
+//		System.out.println("pwd : " + pwd);
+//	
+//		String salt = "jhsdjkfhsadfjksfahafksjsdafhkjsdyisdug";
+//		System.out.println("salt : " + salt);
+//		
+//		
+//		String res = en.getEncrypt(pwd, salt);
+//		
+//		System.out.println(res);
+//	}
+
+}
--- AIproject/src/main/webapp/resources/js/login.js
+++ AIproject/src/main/webapp/resources/js/login.js
@@ -5,13 +5,13 @@ const userPassword = document.querySelector("#loginPassword");
 const loginBTN = document.querySelector("#loginBTN");
 
 // let container = document.getElementById('container')
-// toggle = () => {
-//   container.classList.toggle('sign-in')
-//   container.classList.toggle('sign-up')
-// }
-// setTimeout(() => {
-//   container.classList.add('sign-in')
-// }, 200)
+ toggle = () => {
+   container.classList.toggle('sign-in')
+   container.classList.toggle('sign-up')
+ }
+ setTimeout(() => {
+   container.classList.add('sign-in')
+ }, 200)
 
 
 function gohome() {
