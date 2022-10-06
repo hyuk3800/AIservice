830c5f2149ac733dddb68dbd70e4d7dc6e74b1e6
leeinhyuk135 <leeinhyuk135@naver.com>
2022-10-06 19:55:03 +0900

    json 데이터로 로그인시 보내는것 확인
    json 데이터로 로그인시 보내는것 확인

---
+++ AIproject/src/main/java/com/project/biz/user/chatroom/testJson.java
@@ -0,0 +1,24 @@
+package com.project.biz.user.chatroom;
+
+import com.project.member.MemberVo;
+
+public class testJson {
+	private MemberVo user;
+	private String web;
+	
+	
+	public MemberVo getUser() {
+		return user;
+	}
+	public void setUser(MemberVo user) {
+		this.user = user;
+	}
+	public String getWeb() {
+		return web;
+	}
+	public void setWeb(String web) {
+		this.web = web;
+	}
+	
+	
+}
