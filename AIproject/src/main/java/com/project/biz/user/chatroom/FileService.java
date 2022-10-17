9a6271da4164bd885d1f6ba7497ea3fa036ef2ce
leeinhyuk135 <leeinhyuk135@naver.com>
2022-10-17 16:15:27 +0900

    데이터 저장 위치 등 수정
    데이터 저장 위치 등 수정

--- AIproject/src/main/java/com/project/biz/user/chatroom/FileService.java
+++ AIproject/src/main/java/com/project/biz/user/chatroom/FileService.java
@@ -6,6 +6,8 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
@@ -15,12 +17,30 @@ import org.springframework.web.multipart.MultipartFile;
 public class FileService {
 
 	
-	private String uploadDir = "E:\\upload\\tmp";
 	
-	public void fileUpload(MultipartFile multipartFile) {
+	
+	public void fileUpload(MultipartFile multipartFile, HttpSession session) {
+
+		String uploadDir = session.getServletContext().getRealPath("resources/uploadImg");
 		
 		
-		Path copyOfLocation = Paths.get(uploadDir + File.separator + StringUtils.cleanPath("¾ß¾ß¾ß.jpg"));
+		
+		File Folder = new File(uploadDir);
+		
+		if(!Folder.exists()) {
+			try {
+				Folder.mkdir();
+				System.out.println("False");
+			} catch (Exception e) {
+				// TODO: handle exception
+				e.printStackTrace();
+			}
+			
+		}else {
+			System.out.println("True");
+		}
+		
+		Path copyOfLocation = Paths.get(uploadDir + File.separator + StringUtils.cleanPath(multipartFile.getOriginalFilename()));
 		
 		try {
 			Files.copy(multipartFile.getInputStream(), copyOfLocation, StandardCopyOption.REPLACE_EXISTING);
