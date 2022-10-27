69d3caa63d83a21dd0a47b6a2851ed440639b925
ChaewonAngela <111849343+ChaewonAngela@users.noreply.github.com>
2022-10-28 00:24:09 +0900

    짜잘한 수정들
    짜잘한 수정들

--- AIproject/src/main/webapp/resources/js/home2.js
+++ AIproject/src/main/webapp/resources/js/home2.js
@@ -4,6 +4,7 @@
 	   Swal.fire({
 	     title: '마이페이지는 로그인 후 이용 가능합니다.',
 	     text: "로그인 후 다양한 서비스를 이용해보세요!",
+	     icon : 'warning',
 	     showCancelButton: true,
 	     confirmButtonColor: '#3caaffc4',
 	     cancelButtonColor: '#ffb6c1',
